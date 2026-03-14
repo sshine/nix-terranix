b0ad9aec99d454775301969b954e2901642fc84c
Simon Shine <simon@simonshine.dk>
2026-03-14 01:24:10 +0100

    feat: Add Hcloud volume and attachment, rename SSH key resource

--- modules/terranix.nix
+++ modules/terranix.nix
@@ -34,14 +34,28 @@
       name = "kube.mechanicus.xyz";
       server_type = "cx23";
       location = "nbg1";
-      ssh_keys = [ "\${hcloud_ssh_key.t14_key.id}" ];
+      ssh_keys = [ "\${hcloud_ssh_key.my_key.id}" ];
       public_net = {
         ipv4_enabled = true;
         ipv6_enabled = true;
       };
     };
 
-    resource.hcloud_ssh_key.t14_key = {
+    resource.hcloud_volume.my_volume = {
+      name = "kube-mechanicus-data";
+      size = 10;
+      location = "nbg1";
+    };
+
+    resource.hcloud_volume_attachment.data = {
+      volume_id = "\${hcloud_volume.my_volume.id}";
+      server_id = "\${hcloud_server.my_server.id}";
+      automount = true;
+      # delete_protection = true;
+      # format = "ext4";
+    };
+
+    resource.hcloud_ssh_key.my_key = {
       name = "t14";
       public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICjsn8RBtdJzneALvXNwyrpFzb6GqzZnHRjjvMjXBzPW";
     };
